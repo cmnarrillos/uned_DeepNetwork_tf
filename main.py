35b6f35477cadfa7a4cc509c2318f0175d0df88c
cmnarrillos <cmorenonarrillos@gmail.com>
2023-05-24 02:20:42 +0200

    Reorganize and include conv/pool layers

--- main.py
+++ main.py
@@ -17,52 +17,92 @@ training_data, validation_data, test_data = load_data_shared()
 
 # Convert data to proper inputs
 (train_data, train_labels) = training_data
-train_data_np = train_data.numpy().reshape(-1, 28, 28, 1)
-train_data = tf.convert_to_tensor(train_data_np)
-train_labels = train_labels.numpy()
-train_labels = tf.convert_to_tensor(train_labels)
+train_data = tf.convert_to_tensor(train_data.numpy().reshape(-1, 28, 28, 1))
+train_labels = tf.convert_to_tensor(tf.expand_dims(train_labels, axis=1))
 
 (val_data, val_labels) = validation_data
-val_data_np = val_data.numpy().reshape(-1, 28, 28, 1)
-val_data = tf.convert_to_tensor(val_data_np)
-val_labels = val_labels.numpy()
-val_labels = tf.convert_to_tensor(val_labels)
+val_data = tf.convert_to_tensor(val_data.numpy().reshape(-1, 28, 28, 1))
+val_labels = tf.convert_to_tensor(tf.expand_dims(val_labels, axis=1))
 
 (test_data, test_labels) = test_data
-test_data_np = test_data.numpy().reshape(-1, 28, 28, 1)
-test_data = tf.convert_to_tensor(test_data_np)
-test_labels = test_labels.numpy()
-test_labels = tf.convert_to_tensor(test_labels)
+test_data = tf.convert_to_tensor(test_data.numpy().reshape(-1, 28, 28, 1))
+test_labels = tf.convert_to_tensor(tf.expand_dims(test_labels, axis=1))
 
-# Initialize the neural network model
-model = models.Sequential()
 
-# Add convolutional layers
-# model.add(layers.Conv2D(16, (5, 5), activation='relu', input_shape=(28, 28, 1)))
-# model.add(layers.Conv2D(32, (5, 5), activation='relu'))
-# model.add(layers.Conv2D(64, (5, 5), activation='relu'))
 
-# Flatten the output for the fully connected layer
-model.add(layers.Flatten())
 
-# Add fully connected layer
-model.add(layers.Dense(100, activation='relu', kernel_regularizer=regularizers.l2(5.0/50000)))
 
-# Add softmax layer
-model.add(layers.Dense(10, activation='softmax'))
+# -----------------------------------------------------------------------------
+# # 1st network to train: 1 hidden layer with 100 neurons:
+if False:
+    # Initialize the neural network model
+    print('\n\n\n\n NEW CASE: 1 FullyConnected Layer')
+    print('Architecture: [784, 100, 10]')
+    model = models.Sequential([
+        layers.Flatten(),
+        layers.Dense(100, activation='sigmoid',
+                     kernel_regularizer=regularizers.l2(5.0/50000)),
+        layers.Dense(10, activation='softmax')
+    ])
 
-# Define optimizer and loss function
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.25)
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+    # Define optimizer and loss function
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.25)
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
 
-# Compile the model
-model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
+    # Compile the model
+    model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
 
-# Train the model
-model.fit(train_data, train_labels, batch_size=10, epochs=60, validation_data=(val_data, val_labels))
+    # Train the model
+    model.fit(train_data, train_labels, batch_size=10, epochs=60,
+              validation_data=(val_data, val_labels))
 
-# Test the accuracy on test data
-test_loss, test_accuracy = model.evaluate(test_data, test_labels)
+    # Test the accuracy on test data
+    test_loss, test_accuracy = model.evaluate(test_data, test_labels)
 
-print('Test Loss:', test_loss)
-print('Test Accuracy:', test_accuracy)
+    print('Test Loss:', test_loss)
+    print('Test Accuracy:', test_accuracy)
+
+# -----------------------------------------------------------------------------
+# # 2nd network to train: 1 conv-pool + 1 FC layer
+if True:
+    # Initialize
+    print('\n\n\n\n NEW CASE: Convolutional + Pool + FC Layer')
+    print('Architecture: [784, 20x(24,24), 100, 10]')
+    model = models.Sequential([
+        layers.Conv2D(filters=20, kernel_size=(5, 5),
+                      activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(units=100, activation='sigmoid',
+                     kernel_regularizer=regularizers.l2(5.0/50000)),
+        layers.Dense(10, activation='softmax')
+    ])
+
+    # Define optimizer and loss function
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.25)
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+
+    # Compile the model
+    model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
+
+    # Train the model
+    model.fit(train_data, train_labels, batch_size=10, epochs=60,
+              validation_data=(val_data, val_labels))
+
+    # Test the accuracy on test data
+    test_loss, test_accuracy = model.evaluate(test_data, test_labels)
+
+    print('Test Loss:', test_loss)
+    print('Test Accuracy:', test_accuracy)
+
+
+    # net = Network([
+    #     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+    #                   filter_shape=(20, 1, 5, 5),
+    #                   poolsize=(2, 2)),
+    #     FullyConnectedLayer(n_in=20 * 12 * 12, n_out=100),
+    #     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+    #
+    # # Train the network
+    # net.SGD(training_data, epochs, mini_batch_size, lr,
+    #             validation_data, test_data, lmbda=lmbda)
